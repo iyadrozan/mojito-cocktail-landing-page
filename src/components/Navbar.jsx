c1e519d12ca6f8cd239e8153a43431e373c3bb55
iyadrozan <iyadrozan26@gmail.com>
2025-07-16 21:42:05 +0800

    implement the art section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   return (
     <nav>
         <div>
-            <a href="#home" className="flex items-center gap-2">
+            <a href="#" className="flex items-center gap-2">
                 <img src="/images/logo.png" alt="logo" />
                 <p>Velvet Pour</p>
             </a>
